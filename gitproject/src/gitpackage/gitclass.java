88174f9fb420190f708a0b57cd9b42e7f7c26101
Abhijith <abhijithkannan7332@gmail.com>
2024-07-30 10:43:31 +0530

    first commit

---
+++ gitproject/src/gitpackage/gitclass.java
@@ -0,0 +1,13 @@
+package gitpackage;
+
+public class gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a =45;
+
+int b=75;
+System.out.println("sum " + (a+b));
+}
+
+}
